4ceee4086f18b9f4a3a4b57f437734c6e2c7678d
arjitjohar <arjitjohar@gmail.com>
2025-06-19 14:28:23 -0400

    redploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMRwe
         </p>
         <p> testing new line of code</p>
       </div>
